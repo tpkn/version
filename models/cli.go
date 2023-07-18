14d945081253ca1b0b1a5f4d02ef44505f4e129b
tpkn <tpkn@users.noreply.github.com>
2023-07-18 12:32:06 +0300

    Add files via upload

--- models/cli.go
+++ models/cli.go
@@ -4,5 +4,6 @@ type CLI struct {
 	About    string
 	FilePath string
 	Options  string
+	Version  string
 	Verbose  bool
 }
--- models/version.go
+++ models/version.go
@@ -1,19 +1,27 @@
 package models
 
+import (
+	"fmt"
+)
+
 type Version struct {
 	Major int
 	Minor int
 	Patch int
 }
 
-func (n *Version) IncMajor() {
-	n.Major++
+func (v *Version) IncMajor() {
+	v.Major++
 }
 
-func (n *Version) IncMinor() {
-	n.Minor++
+func (v *Version) IncMinor() {
+	v.Minor++
 }
 
-func (n *Version) IncPatch() {
-	n.Patch++
+func (v *Version) IncPatch() {
+	v.Patch++
+}
+
+func (v *Version) Stringer() string {
+	return fmt.Sprintf("%v.%v.%v", v.Major, v.Minor, v.Patch)
 }
